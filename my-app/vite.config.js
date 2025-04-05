7bcfae8fa6666a09af68ec57d29e82204a22884d
EmilBAD <hat-patfaceit@mail.ru>
2025-04-05 03:37:37 +0300

    v0.3

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Экспортируем конфигурацию
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Подключаем плагин React
   server: {
     proxy: {
       '/api': {
-        target: 'https://kudago.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/public-api/v1.4'),
+        target: 'https://kudago.com', // Целевой сервер
+        changeOrigin: true, // Изменяем заголовок Origin
+        rewrite: (path) => path.replace(/^\/api/, '/public-api/v1.4'), // Переписываем путь
       },
     },
   },
